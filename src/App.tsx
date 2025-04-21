9cab7b3a279fb0e70bbf4e54487e31533742ef70
sipllyyliam <simplyliam.dev@gmail.com>
2025-04-21 12:13:41 +0200

    feat: initialize React application with Vite and Tailwind CSS
    - Added main application structure in App.tsx
    - Integrated Tailwind CSS in index.css
    - Set up Vite configuration for React
    - Created TypeScript configuration files for app and node
    - Added SVG icon for the application
    - Created initial component structure for Dock, Lock Screen, Menubar, Preferences, and Shared components

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-[url('/Bg-1.png')] bg-center bg-cover">
+      
+    </div>
+  )
+}
+
+export default App
