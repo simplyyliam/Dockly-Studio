8344077abd1bf191763ed0666affe18576faa409
sipllyyliam <simplyliam.dev@gmail.com>
2025-04-22 21:35:40 +0200

    feat: enhance Sliders component layout and styling; add CSS for slider functionality

--- src/components/Menubar.tsx/Control Center Components/Sliders.tsx
+++ src/components/Menubar.tsx/Control Center Components/Sliders.tsx
@@ -1,8 +1,15 @@
 
 function Sliders() {
   return (
-    <div className="flex items-center justify-center w-full h-full bg-[#6A6A6A]/25 rounded-2xl">
-        
+    <div className="flex flex-col p-4 gap-4 items-start w-full h-full bg-[#6A6A6A]/25 rounded-2xl">
+        <div className="flex flex-col gap-2 w-full">
+            <h1 className="text-white">Display</h1>
+            <input type="range" className="slider  focus:outline-none" />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+            <h1 className="text-white">Volume</h1>
+            <input type="range" className="slider  focus:outline-none" />
+        </div>
     </div>
   )
 }
